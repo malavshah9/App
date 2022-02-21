a3cdf8641a5425d4b25e28eeff0aaae4f4d59c2a
Malav Shah <mdshah9574@gmail.com>
2022-02-21 22:55:39 +0530

    chore: server setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,23 @@
 import express from "express";
-const app = express();
-const port = 8080; // default port to listen
+import dotenv from "dotenv";
+import bodyparser from "body-parser";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
 
-// define a route handler for the default home page
+dotenv.config({ path: "./.env" });
+const app = express();
+app.use(bodyparser.json());
+app.use(cors());
+app.use(helmet({ hidePoweredBy: true }));
+app.use(morgan("combined"));
+
 app.get("/", (req, res) => {
   res.send("Hello world!");
 });
 
 // start the Express server
+const port = process.env.PORT;
 app.listen(port, () => {
   // tslint:disable-next-line:no-console
   console.log(`server started at http://localhost:${port}`);
